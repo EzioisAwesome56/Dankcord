f3250708d2c3801635defa2066ca494decfc4874
Jon <jedijon567@gmail.com>
2020-10-31 16:11:05 -0400

    start working on registration page

--- src/main/java/com/eziosoft/Dankcord/webAuth/webServer.java
+++ src/main/java/com/eziosoft/Dankcord/webAuth/webServer.java
@@ -7,6 +7,8 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class webServer {
 
@@ -20,21 +22,43 @@ public class webServer {
         System.out.println("Dankcord whweb interface is now starting...");
         // init the http server
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/test", new test());
+        server.createContext("/auth", new authPage());
         server.setExecutor(null);
         server.start();
         System.out.println("Web server has finished loading!");
     }
 
+    // from https://stackoverflow.com/questions/11640025/how-to-obtain-the-query-string-in-a-get-with-java-httpserver-httpexchange
+    private static Map<String, String> queryToMap(String query) {
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
+    }
 
-    static class test implements HttpHandler {
+
+    static class authPage implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            byte[] h = "gamer fuel".getBytes();
+            String text = queryToMap(exchange.getRequestURI().getQuery()).get("a");
+            byte[] h = text.getBytes();
             exchange.sendResponseHeaders(200, h.length);
             OutputStream out = exchange.getResponseBody();
             out.write(h);
             out.close();
+        }
+    }
+
+    static class registerPage implements HttpHandler{
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            // mountian dew gamer fuel
         }
     }
 }
