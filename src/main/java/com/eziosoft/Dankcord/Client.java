9faeb2ccfeea521e83b5aca0a72f354f9ac72500
Jon <jedijon567@gmail.com>
2020-11-01 20:13:20 -0500

    redo client auth using tokens

--- src/main/java/com/eziosoft/Dankcord/Client.java
+++ src/main/java/com/eziosoft/Dankcord/Client.java
@@ -1,6 +1,7 @@
 package com.eziosoft.Dankcord;
 
 
+import com.fasterxml.jackson.databind.ser.std.NullSerializer;
 import com.google.gson.Gson;
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -21,7 +22,7 @@ public class Client {
     private static String get;
     private static String shit;
     private static Gson gson = new Gson();
-    private static byte[] publickey;
+    private static PublicKey publickey;
     private SecureRandom sr = new SecureRandom();
     private int conid;
 
@@ -42,108 +43,67 @@ public class Client {
         Thread loop = new Thread(){
             public void run(){
                 try {
-                    // first we need to establish encryption to the client
-                    out.writeUTF("KEY");
-                    if (in.readUTF().equals("OK")){
-                        // prepare and send the key
-                        KeyPair pair;
-                        try {
-                            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-                            kpg.initialize(2048, sr);
-                            pair = kpg.generateKeyPair();
-                        } catch (NoSuchAlgorithmException e){
-                            e.printStackTrace();
-                            System.out.println("Critical error while trying to enable encryption!");
-                            s.close();
-                            out.close();
-                            in.close();
-                            Server.deleteClient(conid);
-                            return;
-                        }
-                        // store private key for use by server
-                        publickey = pair.getPublic().getEncoded();
-                        // send the client the public key
-                        out.writeUTF(Base64.getEncoder().encodeToString(pair.getPrivate().getEncoded()));
-                    } else {
-                        System.out.println("Client is not acting correctly! GTFO");
+                    // first we need to get the username from the client
+                    out.writeUTF("USER");
+                    String name = in.readUTF();
+                    // check if the account exists
+                    if (!Database.checkForUser(name)){
+                        // account does not exist, disconnect client
+                        out.writeUTF("FAIL");
                         s.close();
                         out.close();
                         in.close();
                         Server.deleteClient(conid);
                         return;
                     }
-                    // try to auth client
+                    // load authBlock object for user
+                    authBlock block = Database.loadAuthBlock(name);
+                    // quickly load public key
+                    try{
+                        publickey = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(block.getkeyb64())));
+                    } catch (Exception e){
+                        // critical error, this should not happen
+                        e.printStackTrace();
+                        System.out.println("HOW DID THIS HAPPEN?");
+                        System.exit(-2);
+                    }
+                    // ask the client for its authentication token
                     out.writeUTF("AUTH");
                     get = in.readUTF();
                     // decrypt this shit
                     try {
                         Cipher cipher = Cipher.getInstance("RSA");
-                        cipher.init(Cipher.DECRYPT_MODE, KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publickey)));
+                        cipher.init(Cipher.DECRYPT_MODE, publickey);
                         shit = new String(cipher.doFinal(Base64.getDecoder().decode(get)));
                     } catch (Exception e){
                         e.printStackTrace();
+                        System.out.println("HOW DID THIS HAPPEN?");
                         System.exit(-2);
                     }
-
-                    // load a user object from the obtained data
-                    User main = gson.fromJson(shit, User.class);
-                    // check if supplied username is in database
-                    if (!Database.checkForUser(main.getUsername())){
-                        // handle user registration
-                        out.writeUTF("REG");
-                        // wait for new data to be written
-                        while (true){
-                            if (in.available() > 0){
-                                get = in.readUTF();
-                                break;
-                            }
-                        }
-                        // decrypt data & make a new user object
-                        User dank = new User("h", "h", "h");
-                        try {
-                            Cipher cipher = Cipher.getInstance("RSA");
-                            cipher.init(Cipher.DECRYPT_MODE, KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publickey)));
-                            shit = new String(cipher.doFinal(Base64.getDecoder().decode(get)));
-                            dank = gson.fromJson(shit, User.class);
-                        } catch (Exception e){
-                            e.printStackTrace();
-                            System.exit(-2);
-                        }
-                        // this user object is awful tho, we need to hash the password first
-                        // to do this, first we need a SALT
-                        String salt = BCrypt.gensalt();
-                        // then we can hash the password
-                        String hash = BCrypt.hashpw(dank.getPasshash(), salt);
-                        // then load all this into a new user object
-                        dank = new User(dank.getUsername(), hash, salt);
-                        // dump this shit into the database
-                        Database.saveUser(dank);
-
-                        // finally done! send ok
-                        out.writeUTF("OK");
-                    } else {
-                        // get password from object
-                        String pass = main.getPasshash();
-                        // load user account from database
-                        main = Database.loadUser(main.getUsername());
-                        // check if the hashes match
-                        if (!BCrypt.hashpw(pass, main.getSalt()).equals(main.getPasshash())){
-                            // they dont match! GTFO
-                            out.writeUTF("FAIL");
-                            s.close();
-                            out.close();
-                            in.close();
-                            Server.deleteClient(conid);
-                            return;
-                        } else {
-                            // send the ok
-                            out.writeUTF("OK");
-                        }
+                    // compare the auth token with the one in the database
+                    if (!shit.equals(block.getToken())){
+                        // token invalid! disconnect client
+                        out.writeUTF("FAIL");
+                        s.close();
+                        out.close();
+                        in.close();
+                        Server.deleteClient(conid);
+                        return;
                     }
+                    // if we are here, it means the client checks out as legitimate
+                    out.writeUTF("OK");
+
                     // main loop
                     while (true){
                         get = in.readUTF();
-                        if (get.equals("DC")){
+                        try{
+                            Cipher cipher = Cipher.getInstance("RSA");
+                            cipher.init(Cipher.DECRYPT_MODE, publickey);
+                            shit = new String(cipher.doFinal(Base64.getDecoder().decode(get)));
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+                        if (shit.equals("DC")){
                             s.close();
                             System.out.println("Client has disconnected!");
                             break;
